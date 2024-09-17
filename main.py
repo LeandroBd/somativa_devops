40287c3928ad5a09582a06cd00af7b74da9edbe2
LeandroBd <leandrodossantos_19@hotmail.com>
2024-09-16 22:57:19 -0300

    resolver conflito

--- main.py
+++ main.py
@@ -9,8 +9,4 @@ async def root():
 
 @app.get("/teste4")
 async def funcaoteste():
-<<<<<<< HEAD
     return {"teste": True, "Num_aleatório ": random.randint(0, 50000)}
-=======
-    return {"teste": "Teste ok"}
->>>>>>> cf8907c4b5ca25571b5df6a49b32284de4dbca21
